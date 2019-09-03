49c61ef4b918aabafb0d50ddbeed11f2ee70e2d3
mltomlins0n <martin070891@gmail.com>
2019-09-03 13:02:34 +0100

    Add functions to manipulate on screen element

--- test.js
+++ test.js
@@ -1,11 +1,112 @@
+// Useful Functions
+//document.getElementById();
+//document.createElement();
+
 console.log("Hello World");
-window.alert("HALLOW WORLD");
-document.write("Written using document.write"); // Writes directly to the webpage
-console.warn("I wouldn't..."); // Adds a warning to the console
-console.error("You fucked up");// Adds an error to the console
+//window.alert("HALLOW WORLD");
+//document.write("Written using document.write"); // Writes directly to the webpage
+//console.warn("I wouldn't..."); // Adds a warning to the console
+//console.error("You fucked up");// Adds an error to the console
 
 let message = "This is a variable message";
 console.log (message);
+
+function handleClick(element) {
+    alert("WHY CLICK THIS?!");
+    //element.style.backgroundColor = "black";
+    //element.innerText = "?!?!?!?!?!?!?!?!?!?!";
+}
+
+const containerElement = document.getElementById("container");
+const newElement = document.createElement("h1");
+newElement.id="number";
+newElement.innerText = "0";
+containerElement.appendChild(newElement);
+
+const newButton = document.createElement("button");
+newButton.className="numButton";
+newButton.innerText = "-10";
+containerElement.append(newButton);
+newButton.addEventListener("click", () => {
+    subTen();
+});
+
+const newButton2 = document.createElement("button");
+newButton2.className="numButton";
+newButton2.innerText = "-1";
+containerElement.append(newButton2);  
+newButton2.addEventListener("click", () => {
+    decrement();
+});
+
+const newButton3 = document.createElement("button");
+newButton3.className="numButton";
+newButton3.innerText = "Reset";
+containerElement.append(newButton3);
+newButton3.addEventListener("click", () => {
+    reset();
+});
+
+const newButton4 = document.createElement("button");
+newButton4.className="numButton";
+newButton4.innerText = "+1";
+containerElement.append(newButton4);  
+newButton4.addEventListener("click", () => {
+    increment();
+});
+
+const newButton5 = document.createElement("button");
+newButton5.className="numButton";
+newButton5.innerText = "+10";
+containerElement.append(newButton5);  
+newButton5.addEventListener("click", () => {
+    addTen();
+});
+
+let counter = 0;
+let lastCounter = 0;
+
+function updateCounter() {
+    newElement.innerText = counter;
+    if (counter %2 == 0){
+        document.getElementById("number").style.backgroundColor="green";
+    } else {
+        document.getElementById("number").style.backgroundColor="red";
+    }
+    if (counter > 100) {
+        document.getElementById("number").innerText=counter + "!";
+    }
+}
+
+function increment() {
+    counter++;
+    updateCounter();
+}
+
+function decrement() {
+    counter--;
+    updateCounter();
+}
+
+function addTen() {
+    counter += 10;
+    updateCounter();
+}
+
+function subTen() {
+    counter -= 10;
+    updateCounter();
+}
+
+function reset() {
+    counter = 0;
+    updateCounter(0);
+}
+
+function counterHistory() {
+    lastcounter = counter;
+    console.log(lastCounter);
+}
 
 function messageFunc() {
     console.log("This came from a function");
@@ -88,7 +189,7 @@ let filteredAnimals = "Animals: " + animals.filter((value, index) => {
 });
 
 const betterFilteredAnimals = animals
-    .filter(item => item.length <9)
+    .filter(item => item.length < 9)
     .map (item => item[0].toUpperCase() + item.slice(1))
     .map ((item, index) => index ? ", " + item : item) // True: add comma. False: just add item
     .reduce ((prev, current) => prev + current, "Animals: ") // "Animals: " is the initial value of "prev"
