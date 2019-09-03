54a24584af3ce0d6362bc8a11af51ed7d2134eb1
mltomlins0n <martin070891@gmail.com>
2019-09-03 11:19:30 +0100

    Add JS functions to manipulate arrays

--- test.js
+++ test.js
@@ -13,21 +13,23 @@ function messageFunc() {
 messageFunc();
 
 function getArgs(){
-    return arguments; // gets all args passed into it
+    return arguments; // gets all args passed into function
 }
 
-const calculate = (operator, num1, num2) => { // lambda function
-    if (operator == "+"){
-        return num1 + num2;
-    }
-    else if (operator == "-"){
-        return num1 - num2;
-    }
-    else if (operator == "/"){
-        return num1/num2;
-    }
-    else if (operator == "*"){
-        return num1 * num2;
+function calculator(operator, num1, num2) {
+    const add = () => num1 + num2; // lambda function
+    const sub = () => num1 - num2;
+    const multi = () => num1 * num2;
+    const div = () => num1 / num2;
+
+    if (operator == "+") {
+        return add();
+    } else if (operator == "-") {
+        return sub();
+    } else if (operator == "/") {
+        return div();
+    } else if (operator == "*") {
+        return multi();
     } else {
         console.error("Invalid selection, try again");
     }
@@ -55,3 +57,42 @@ function multiplyAll() {
     }
     console.log(answer);
 }
+
+let myArray = ["a", "b", "c"];
+
+function printArray() {
+    myArray.forEach((value, index, array) => {
+    console.log("Value: " + value + " Index: " + index + " Array: " + array);
+});
+}
+//printArray();
+
+const animals = ["frog", "llama", "elephant", "crocodile", "hippopotamus", 
+                 "aardvark", "geoff", "galapagos giant tortoise"];
+
+let filteredAnimals = "Animals: " + animals.filter((value, index) => {
+    if (value.length >= 9) { // filter out words that are 9 chars or longer
+        return false;
+    } else {
+        return true;
+    }
+}).map((value, index, array) => {
+    if (index < array.length -1) {
+        return value[0].toUpperCase() + value.substring(1) + ", ";
+    } else {
+        return value[0].toUpperCase() + value.substring(1) + ".";
+        
+    }
+}).reduce((prev, current) => {
+    return prev + current;
+});
+
+const betterFilteredAnimals = animals
+    .filter(item => item.length <9)
+    .map (item => item[0].toUpperCase() + item.slice(1))
+    .map ((item, index) => index ? ", " + item : item) // True: add comma. False: just add item
+    .reduce ((prev, current) => prev + current, "Animals: ") // "Animals: " is the initial value of "prev"
+    .concat(".");
+
+console.log(filteredAnimals);
+console.log(betterFilteredAnimals);
